fd861da5f93988194265cdea52d603a093cc1f9e
emaahh <celestinoemanuele015@gmail.com>
2024-02-06 12:26:46 +0100

    init

--- app/page.js
+++ app/page.js
@@ -1,113 +1,150 @@
+"use client"
 import Image from 'next/image'
+import { useState, useEffect } from 'react';
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('https://cosplaya.pockethost.io');
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const [logged, setLogged] = useState(pb.authStore.isValid)
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+          ...prevState,
+          [name]: value
+        }));
+      };
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            "username": formData.username,
+            "email": formData.email,
+            "emailVisibility": true,
+            "password": formData.password,
+            "passwordConfirm": formData.password,
+            "name": formData.username
+        };
+        const record = await pb.collection('users').create(data);
+        const authData = await pb.collection('users').authWithPassword(formData.email, formData.password);
+        setLogged(pb.authStore.isValid)
+    };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    const handleSubmitLog = async (e) => {
+        e.preventDefault();
+        const authData = await pb.collection('users').authWithPassword(formData.email, formData.password);
+        setLogged(pb.authStore.isValid)
+    };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-14">
+            <div>
+                <h1 className='font-sans font-extrabold text-2xl'>Benvenuto in CosPlaya!</h1>
+            </div>
+
+            {
+                logged?
+
+                <div>
+                    <h1>Loggato come {pb.authStore.model.username}</h1>
+                    <button type="submit" className="w-full bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-700" onClick={()=>{pb.authStore.clear(), setLogged(pb.authStore.isValid)}}>Esci</button>
+                </div>
+
+                :
+                
+                <div>
+                    {/*registrazione*/}
+                    <div className=" mx-auto m-5 bg-white p-8 rounded-[15px] shadow-md">
+                        <h2 className="text-2xl font-bold mb-4 text-gray-700">Registrazione</h2>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-4">
+                            <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Username</label>
+                            <input
+                                type="text"
+                                id="username"
+                                name="username"
+                                value={formData.username}
+                                onChange={handleChange}
+                                className="text-gray-700 w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                required
+                            />
+                            </div>
+                            <div className="mb-4">
+                            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="text-gray-700 w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                required
+                            />
+                            </div>
+                            <div className="mb-4">
+                            <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                className="text-gray-700 w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                required
+                            />
+                            </div>
+                            <button type="submit" className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
+                            Registrati
+                            </button>
+                        </form>
+                    </div>
+                    {/*login*/}
+                    <div className=" mx-auto m-5 bg-white p-8 rounded-[15px] shadow-md">
+                    <h2 className="text-2xl font-bold mb-4 text-gray-700">Log</h2>
+                    <form onSubmit={handleSubmitLog}>
+                        <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="text-gray-700 w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                            required
+                        />
+                        </div>
+                        <div className="mb-4">
+                        <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            className="text-gray-700 w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                            required
+                        />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
+                        Loggati
+                        </button>
+                    </form>
+                    </div>
+
+                </div>
+                
+            }
+
+        </main>
+    )
 }
